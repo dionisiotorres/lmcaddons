9d502988eacddea7fe5da53edc5e87280f2bdcd5
ara-odoo <ara.openerp@gmail.com>
2020-01-28 14:40:48 +0530

    remove rennfield from view

--- website_lmc/controllers/main.py
+++ website_lmc/controllers/main.py
@@ -302,12 +302,11 @@ class CustomerUserPortal(CustomerPortal):
 
     @route(["/racefields"], type="http", auth="public", website=True)
     def racefields(self, **post):
-        rennfelder_ids = request.env['rennfelder'].search([])
-        partner_ids = request.env['res.partner'].sudo().search([])
-        # values = {'rennfelder_ids': rennfelder_ids,
-        #           'partner_ids': partner_ids,
-        #           'company': request.env.user.company_id}
-        values = {}
+        rennfelder_ids = request.env['rennfelder'].search([('id','=',22222200)])
+        partner_ids = request.env['res.partner'].sudo().search([('id','=',22222200)])
+        values = {'rennfelder_ids': rennfelder_ids,
+                  'partner_ids': partner_ids,
+                  'company': request.env.user.company_id}
         return request.render("website_lmc.racefields_lmc", values)
 
 
