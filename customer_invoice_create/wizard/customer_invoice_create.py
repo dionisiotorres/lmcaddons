290065accb737ff4994acf6332ba518458e6cd97
ara-odoo <ara.openerp@gmail.com>
2020-03-10 19:44:04 +0530

    added payment term update

--- customer_invoice_create/wizard/customer_invoice_create.py
+++ customer_invoice_create/wizard/customer_invoice_create.py
@@ -11,7 +11,7 @@ class CustomerInvoiceCreate(models.TransientModel):
 
     @api.multi
     def create_invoice(self):
-        property_payment_term_id = False
+        #property_payment_term_id = False
         context = dict(self._context or {})
         invoice = self.env['account.invoice']
         invoice_line = self.env['account.invoice.line']
@@ -20,17 +20,17 @@ class CustomerInvoiceCreate(models.TransientModel):
             ('type', '=', 'sale')], limit=1)
         for record in self:
             for parnter in partner_ids:
-                if parnter.customer:
-                    property_payment_term_id = parnter.property_payment_term_id
-                if parnter.supplier:
-                    property_payment_term_id = parnter.property_supplier_payment_term_id
+                # if parnter.customer:
+                #     property_payment_term_id = parnter.property_payment_term_id
+                # if parnter.supplier:
+                #     property_payment_term_id = parnter.property_supplier_payment_term_id
                 invoices = invoice.create({
                     'partner_id': parnter.id,
                     'partner_shipping_id': parnter.id,
-                    'date_invoice': self.date_invoice,
+                    'date_invoice': record.date_invoice,
                     'type': 'out_invoice',
                     'journal_id': sale_journal.id,
-                    'payment_term_id': self.payment_term_id.id or property_payment_term_id.id,
+                    'payment_term_id': record.payment_term_id.id,
                     'account_id': parnter.property_account_receivable_id.id,
                 })
                 inv_line = None
