4e9453270fe1d4ccdb3a58ca0e86ababcec1419a
ara-odoo <ara.openerp@gmail.com>
2020-06-05 11:51:47 +0530

    mail will be sent if event invoice sent out

--- website_event_sale_invoice/models/payment.py
+++ website_event_sale_invoice/models/payment.py
@@ -9,17 +9,6 @@ class PaymentTransaction(models.Model):
         contextual_self = self.with_context(block_force_quotation_send=True)
         res = super(PaymentTransaction, contextual_self)._transfer_form_validate(data)
         self._invoice_event_sale_orders()
-        default_template = self.env.ref('account.email_template_edi_invoice')
-        if default_template:
-            if self.invoice_ids:
-                ctx_company = {
-                    'company_id': self.acquirer_id.company_id.id,
-                    'force_company': self.acquirer_id.company_id.id,
-                    'mark_invoice_as_sent': True,
-                }
-                trans = self.with_context(ctx_company)
-                for invoice in trans.invoice_ids:
-                    invoice.message_post_with_template(int(default_template), notif_layout="mail.mail_notification_paynow")
         return res
 
     @api.multi
@@ -38,3 +27,13 @@ class PaymentTransaction(models.Model):
                 invoices = trans.invoice_ids.filtered(lambda inv: inv.state == 'draft')
                 if any([order.order_line.mapped('event_id').filtered(lambda x: x.auto_invoice_validate) for order in trans.sale_order_ids]):
                     invoices.action_invoice_open()
+                    default_template = self.env.ref('account.email_template_edi_invoice')
+                    if default_template:
+                        if trans.invoice_ids:
+                            ctx_company = {
+                                'company_id': trans.acquirer_id.company_id.id,
+                                'force_company': trans.acquirer_id.company_id.id,
+                                'mark_invoice_as_sent': True,
+                            }
+                            for invoice in trans.invoice_ids:
+                                invoice.message_post_with_template(int(default_template), notif_layout="mail.mail_notification_paynow")
